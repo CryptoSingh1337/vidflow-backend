cd4d6e2d875c0bbbe744a2f7f1dfd2cc352ebc4a
Saransh Kumar <cryptosingh@gmail.com>
2021-09-21 20:56:46 +0530

    Closes #1: Initialize project.

---
+++ ConfigServer/src/test/java/com/saransh/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saransh.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
