d5b9e697a137eb81f90b041b5495bc20a609eea1
Saransh Kumar <cryptosingh@gmail.com>
2021-10-12 12:12:32 +0530

    Closes #4: Create Configuration class for Spring Security in Users-ws.

--- UserService/src/main/java/com/saransh/userservice/UserServiceApplication.java
+++ UserService/src/main/java/com/saransh/userservice/UserServiceApplication.java
@@ -2,6 +2,9 @@ package com.saransh.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -10,4 +13,8 @@ public class UserServiceApplication {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
