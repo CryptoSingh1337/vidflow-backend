f9fa000985ac715a6b02aed3845116b322129628
Saransh Kumar <cryptosingh@gmail.com>
2021-12-02 21:30:21 +0530

    Closes #18: Migrate the whole architecture to Monolith.

--- UserService/src/main/java/com/saransh/userservice/model/response/RefreshTokenResponseModel.java
+++
@@ -1,7 +0,0 @@
-package com.saransh.userservice.model.response;
-
-/**
- * Created by CryptSingh1337 on 10/13/2021
- */
-public class RefreshTokenResponseModel extends SuccessfulLoginResponseModel {
-}
---
+++ src/test/java/com/saransh/vidflow/VidFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saransh.vidflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
