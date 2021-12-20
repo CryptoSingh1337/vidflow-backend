be6ee22d1aa2c1f3be421c9317e1e42c0822a9a6
Saransh Kumar <cryptosingh@gmail.com>
2021-12-20 19:52:52 +0530

    Add global cors configuration.

---
+++ src/main/java/com/saransh/vidflow/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.saransh.vidflow.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * author: CryptoSingh1337
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000/");
+    }
+}
