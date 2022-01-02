1aa950fa4e8a3fb4610cb4f44337e9bcb5456d6c
Saransh Kumar <cryptosingh@gmail.com>
2022-01-02 13:22:12 +0530

    Updated CORS origin.

--- src/main/java/com/saransh/vidflow/config/WebConfig.java
+++ src/main/java/com/saransh/vidflow/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000/")
+                .allowedOrigins("https://vidflow.vercel.app/")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
