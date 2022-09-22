2a35a299de9d06146fe317ae7f5637aa3ed4454e
Saransh Kumar <cryptosingh@gmail.com>
2022-09-22 18:49:14 +0530

    Fix #30: Setup multi module project

---
+++ VidFlowWeb/src/main/java/com/saransh/vidflowweb/VidFlowWebApplication.java
@@ -0,0 +1,13 @@
+package com.saransh.vidflowweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidFlowWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VidFlowWebApplication.class, args);
+    }
+
+}
