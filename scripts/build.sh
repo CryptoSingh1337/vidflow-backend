e364fafb1588f97128297e0df9eb20b8bda29217
Saransh Kumar <cryptosingh@gmail.com>
2023-03-11 20:29:11 +0530

    Modify build and start script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,7 @@ mvn -f vidflow-backend/pom.xml clean package
 echo "############# FINISHED #############"
 
 echo "############# COPYING JAR to /var/spring_boot #############"
+sudo mkdir /var/spring_boot
 sudo cp vidflow-backend/VidFlowWeb/target/VidFlowWeb-1.0.jar /var/spring_boot/app.jar
 sudo chmod +x /var/spring_boot/app.jar
 echo "############# FINISHED #############"
